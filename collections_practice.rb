8a70b2d2bbd91da0c3395878b2eb6d81e9dad162
John Guest <jcguest90@gmail.com>
2019-08-24 04:43:04 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -20,14 +20,16 @@ end
 
 
 def sort_array_char_count(strings)
-  strings.sort do |a, b|
-  if a.length == b.length
-    0
-  elsif a.length < b.length
-    -1
-  else a.length > b.length
-    1
-    end
-  end
+  # strings.sort do |a, b|
+  # if a.length == b.length
+  #   0
+  # elsif a.length < b.length
+  #   -1
+  # else a.length > b.length
+  #   1
+  #   end
+  # end
   
+  integers.sort
+  a.lenght <=> b.length
 end   
